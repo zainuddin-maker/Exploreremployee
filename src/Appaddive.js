6d8fe22ae820b0401702032fe1d00796e12f74d5
zainuddin-maker <zainuddineffendie022@gmail.com>
2021-02-18 14:14:38 +0700

    berubah

---
+++ src/Appaddive.js
@@ -0,0 +1,346 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import './appaddive.css';
+import axios from 'axios';
+
+
+export default  class Displayreport extends React.Component {
+
+    constructor(props) {
+        super (props);
+        this.state = {
+
+          nameofemployee :"",
+          jobemployee :"",
+          datasubordinates : [],
+          datanondirectsubordinates : [],
+          close : true ,
+          count  :0 ,
+
+          employeeparent :[],
+
+          allemploye :[
+            "John Hartman",
+            "Samad Pitt",
+            "Amaya Knight",
+            "Leanna Hogg",
+            "Aila Hodgson",
+            "Vincent Todd",
+            "Faye Oneill",
+            "Lynn Haigh",
+            "Nylah Riddle"
+          ]
+
+
+      }
+      this.Submitname = this.Submitname.bind(this)
+    }
+
+    Submitname(){
+      this.setState( { datasubordinates : []})
+      this.setState( { employeeparent :[] })
+      this.setState( {datanondirectsubordinates : []})
+      this.setState({count : 0})
+
+      axios.get('http://api.additivasia.io/api/v1/assignment/employees/'+ this.state.nameofemployee
+      )
+              .then(res => {
+                 if (res.data[0]=== "employee"){
+
+                    this.setState({datasubordinates : [] ,close : false})
+                    this.setState({employeeparent : [this.state.nameofemployee] })
+                    this.setState({jobemployee : res.data[0]})
+                  }else {
+                    this.setState({datasubordinates : res.data[1]["direct-subordinates"] ,close : false })
+                    this.setState({employeeparent : res.data[1]["direct-subordinates"].concat(this.state.nameofemployee) })
+                    this.setState({jobemployee : res.data[0]})
+
+                  }
+                  
+
+
+                  this.state.allemploye.forEach((allemploye)=>{
+                    axios.get('http://api.additivasia.io/api/v1/assignment/employees/'+ allemploye
+                    )
+                    .then(res => {
+                 
+                      this.setState({count : 0})
+                     
+                      if(allemploye !== this.state.nameofemployee){
+
+                        this.state.employeeparent.forEach((employeeparent)=>{
+
+                          if(allemploye !== employeeparent){
+
+                            this.setState({count : this.state.count + 1})
+                           
+                            if (this.state.count === this.state.employeeparent.length ){
+
+                            this.setState({datanondirectsubordinates : this.state.datanondirectsubordinates.concat(allemploye)})
+                            this.setState({count : 0})
+                            }
+                            
+
+                          }
+
+                      })
+
+
+
+                      }
+
+                        if (res.data[0] ==="employee"){
+                          this.setState({count : 0})
+                          this.state.employeeparent.forEach((employeeparent)=>{
+
+                              if(allemploye !== employeeparent){
+
+                                this.setState({count : this.state.count + 1})
+                               
+                                if (this.state.count === this.state.employeeparent.length ){
+
+                                this.setState({datanondirectsubordinates : this.state.datanondirectsubordinates.concat(allemploye)})
+                                this.setState({count : 0})
+                                }
+                                
+
+                              }
+
+                          })
+                      }else {
+
+                        res.data[1]["direct-subordinates"].forEach((directsubordinates)=>{
+                          this.setState({count : 0})
+                                  this.state.employeeparent.forEach((employeeparent)=>{
+
+                                    if(directsubordinates !== employeeparent){
+
+                                      this.setState({count : this.state.count + 1})
+                                      
+                                      if (this.state.count === this.state.employeeparent.length  ){
+                                        
+                                      this.setState({datanondirectsubordinates : this.state.datanondirectsubordinates.concat(directsubordinates)})
+                                      this.setState({count : 0})
+                                      }
+                                      
+
+                                    }
+
+                                })
+
+                                
+                        })
+                          
+
+                      }
+
+
+                      
+                 
+                    
+
+
+                    })
+                    .catch(error => {
+                      console.log("error", error);
+                      
+                    });
+
+
+                  })
+
+
+
+
+
+              })
+              .catch(error => {
+                  console.log("error", error);
+
+                  this.setState({close : true})
+                  
+              });
+
+              this.setState({count : 0})
+    }
+
+      componentDidMount() {
+
+       
+
+        axios.get('http://api.additivasia.io/api/v1/assignment/employees'
+        )
+                .then(res => {
+                    // console.log(res.data);
+                   
+  
+                })
+                .catch(error => {
+                    console.log("logout error", error);
+                    
+                });
+       
+         
+
+
+
+
+      }
+
+
+      
+      
+  
+
+    render() {
+
+    //  console.log(this.state.datanondirectsubordinates.filter(onlyUnique))
+       
+
+
+       
+        return (
+            
+            <React.Fragment>
+
+              <Grid>
+                    <Grid container justify="center" alignItems="center" style={{height:"50px",backgroundColor:""}}>
+                      <div className="employeeexplorer">
+                         Employee Explorer
+                      </div>
+                     
+                    </Grid>
+                    <Grid container justify="center" alignItems="center" style={{height:"50px"}}>
+
+                        <Grid container direction="row" style={{width:"400px",backgroundColor:""}}>
+
+                            <input  
+                                                  type="text"
+                                                  name="confirmyear" 
+                                                  placeholder="what year?" 
+                                                  className="kotakinputprofileemploye"
+                                                  value={this.state.nameofemployee}
+                                                  onChange={e => {
+                                                    this.setState({ nameofemployee : e.target.value });
+
+                                                    }}
+                                                    style={{height:"50px"}}
+                                                    /> 
+
+                            <div onClick={this.Submitname} className="buttonlistemployee">
+                                  Search
+                            </div>
+
+                        </Grid>
+                        {
+                           this.state.close || this.state.nameofemployee ==="" ?
+
+                          null
+                          :
+                          <>
+                        <Grid container justify="center" alignItems="center" style={{height:"",backgroundColor:""}}>
+
+                             <Grid container direction="row" style={{width:"400px",backgroundColor:""}}>
+
+                             
+                              <div className="employeeexplorer" style={{fontWeight:"500",fontSize:"15px"}}>
+                              Subordinates of employee {this.state.nameofemployee} ({this.state.jobemployee}):
+                              </div>
+
+                               
+
+                             
+
+                               </Grid>
+                             
+                     
+                          </Grid>
+                             <Grid container justify="center" alignItems="center" style={{height:"",backgroundColor:""}}>
+
+                               {   this.state.datasubordinates.length === 0?
+                                 <Grid container direction="column" style={{width:"400px",backgroundColor:""}}>
+                                <div className="employeeexplorer" style={{fontWeight:"400",fontSize:"13px"}}>
+                                     -
+                                 </div>
+                                 </Grid>
+                                 :
+                                 
+                                 <Grid container direction="column" style={{width:"400px",backgroundColor:""}}>
+   
+                                 {
+                                                                    this.state.datasubordinates.map((data,i)=>
+                                                                    <div key={i} className="employeeexplorer" style={{fontWeight:"400",fontSize:"13px"}}>
+                                                                    {data}
+                                                                     </div>
+                                                                    )
+                                                                }
+       
+                                   </Grid>
+
+
+                                } 
+                        
+   
+                          </Grid>
+                                         <Grid container justify="center" alignItems="center" style={{height:"20px",backgroundColor:""}}>
+
+                             <Grid container direction="row" style={{width:"400px",backgroundColor:""}}>
+
+                             <div className="employeeexplorer" style={{fontWeight:"500",fontSize:"15px"}}>
+                               Nondirect Subordinates of employee {this.state.nameofemployee} ({this.state.jobemployee}):
+                              </div>
+
+                               </Grid>
+
+                               <Grid container justify="center" alignItems="center" style={{height:"",backgroundColor:""}}>
+                             <Grid container direction="column" style={{width:"400px",backgroundColor:""}}>
+   
+                             {
+                                                                this.state.datanondirectsubordinates.filter(onlyUnique).map((data,i)=>
+                                                                <div key={i} className="employeeexplorer" style={{fontWeight:"400",fontSize:"13px"}}>
+                                                                {data}
+                                                                 </div>
+                                                                )
+                                                            }
+   
+                               </Grid>
+   
+                          </Grid>
+
+
+                             
+                     
+                          </Grid>
+                          </>
+                          }
+
+                  
+                     
+                                     
+
+                     
+                    </Grid>
+              </Grid>
+ 
+                          
+                                
+            </React.Fragment>
+        );
+    }
+}
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+
+
+
+  
+
+
+
+
+
+
+
+
--- src/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UserSession } from "./UserSession";
-export * from "./Storage";
-export * from "./HttpClient";
-export { default as history } from "./History";
